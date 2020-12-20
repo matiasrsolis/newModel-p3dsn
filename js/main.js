ce9b5f6a2d77b6388759c84b7e99435038d4dd74
Matias <matiasrsolis@yahoo.com.ar>
2020-12-20 18:57:06 -0300

    Importing JQuery

--- js/main.js
+++ js/main.js
@@ -1,5 +1,15 @@
-var myCarousel = document.querySelector('#myCarousel')
-var carousel = new bootstrap.Carousel(myCarousel, {
-  interval: 2000,
-  wrap: false
-})
+$(document).ready(function() {
+     $(window).on('scroll', function(){
+        if($(window).scrollTop()){
+            $('#isologo').css({"display":"block"});
+            $('#logo').css({"display":"none"});
+            $('.escudo').css({"display":"none"});
+            //$('.headerBox nav').css({"flex-direction":"row"});
+        } else {
+            $('#isologo').css({"display":"none"});
+            $('#logo').css({"display":"block"});
+            $('.escudo').css({"display":"block"});
+            //$('.headerBox nav').css({"flex-direction":"column-reverse"});
+        }
+    });
+});
